a47f798e18baced562ce753fcac9c742ff1c3942
tuantuana <thuytrang5962@gmail.com>
2026-08-17 22:14:51 +0700

    TUAN

--- services/fetchAllData.js
+++ services/fetchAllData.js
@@ -15,10 +15,10 @@ async function fetchAllData() {
     const allData = {};
 
 
-const now = new Date(); // Lấy thời gian hiện tại
-const nowTimestamp = Math.floor(now.getTime() / 1000); // Unix timestamp hiện tại
-const from = Math.floor(nowTimestamp / 3600) * 3600; // Làm tròn về đầu giờ
-const to = from; // Thời gian hiện tại
+const nowTimestamp = Math.floor(Date.now() / 1000);
+    const currentHour = Math.floor(nowTimestamp / 3600) * 3600;
+    const from = currentHour - 3600; // 19:00
+    const to = from; 
 
 
 
